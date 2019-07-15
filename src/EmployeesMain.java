e23709b7f73e8c1835971f7480c22b47728168f5
Marija Trajkova <tmarija2008@yahoo.com>
2019-07-15 11:46:38 +0200

    Add homework solution

---
+++ src/EmployeesMain.java
@@ -0,0 +1,27 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EmployeesMain {
+    public static void main(String[] args) {
+        List<Employee> employeeList = GenerateEmployees.generateEmployees(30);
+
+        System.out.println("NUMBER OF BITOLA EMPLOYEES WITH SALARY > 800");
+        System.out.println(employeeList.stream().filter(e -> e.getCity().equals("Bitola") && e.getSalary() > 800).count());
+
+        System.out.println("\nTOTAL SALARY OF SKOPJE EMLOYEES");
+        int salary = employeeList.stream().filter(e -> e.getCity().equals("Skopje")).mapToInt(Employee::getSalary).sum();
+        System.out.println(salary);
+
+        System.out.println("\nEMPLOYEES SORTED BY SALARY");
+        employeeList.stream().sorted(Comparator.comparingInt(Employee::getSalary)).collect(Collectors.toList()).forEach(System.out::println);
+
+
+        System.out.println("\nEMPLOYEE FROM SKOPJE THAT HAS THE LARGEST SALARY");
+        Employee richestSkopjeEmployee = employeeList.stream().filter(e -> e.getCity().equals("Skopje")).sorted(Comparator.comparingInt(Employee::getSalary).reversed()).findFirst().get();
+        //OR
+        employeeList.stream().filter(e -> e.getCity().equals("Skopje")).max(Comparator.comparingInt(Employee::getSalary)).ifPresent(e -> System.out.println(e.getSalary()));
+        System.out.println(richestSkopjeEmployee.getSalary());
+    }
+
+}
